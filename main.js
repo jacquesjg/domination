aa2c3a409d70159eb18102f7f80d1b6830e07931
jacquesjg <jjeangi@gmail.com>
2021-07-06 17:05:44 +0200

    submission

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ console.log('li', li)
 
 strikethrough();
 
+/////////////////
+
+function setImage(id, url){
+    const img = document.querySelector(id);
+    img.height = 250;
+    img.src = url;
+}
+
+setImage('#image-1', '')
+
+////////////
+
+function removeFirstLi(){
+    const removeFirst = document.querySelector('#arguments li');
+    removeFirst.remove();
+}
+
+removeFirstLi();
+removeFirstLi();
+
+////////////
+
+function changeSize(id, size){
+    const changeSize = document.querySelector(id);
+    changeSize.style.fontSize = size
+}
+
+changeSize('#thing-2', '28px')
